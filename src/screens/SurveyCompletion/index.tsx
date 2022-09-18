da4231e366668a41db079d76cc5b66aedf7f2b38
Nation Shakya <nation@coresoftint.com>
2022-09-18 22:05:04 +0545

    UXC - Survey completion page

--- src/screens/SurveyCompletion/index.tsx
+++ src/screens/SurveyCompletion/index.tsx
@@ -1,8 +1,22 @@
 // components
+import SpinLoader from "components/SpinLoader";
 import { QuestionLayout } from "components/Layouts";
 
+// hooks
+import useFetch from "hooks/useFetch";
+
 const SurveyCompletion = () => {
-  return <QuestionLayout title="SurveyCompletion" disablePageCount />;
+  const { response, isLoading, error } = useFetch("end-page");
+
+  if (error) {
+    return <p>Error while fetching data. Please try again later.</p>;
+  }
+
+  if (isLoading) {
+    return <SpinLoader />;
+  }
+
+  return <QuestionLayout title={response?.title} disablePageCount />;
 };
 
 export default SurveyCompletion;
