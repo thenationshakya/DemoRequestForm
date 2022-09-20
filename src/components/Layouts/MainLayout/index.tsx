233a022b6a35a5cf9c78845ae92e355c9fda72e5
Nation Shakya <nation@coresoftint.com>
2022-09-20 22:15:40 +0545

    UXC - Progress bar width formula

--- src/components/Layouts/MainLayout/index.tsx
+++ src/components/Layouts/MainLayout/index.tsx
@@ -7,39 +7,15 @@ import { MainLayoutProps } from "./types";
 const MainLayout = ({ children }: MainLayoutProps) => {
   const { page } = usePageState();
 
-  function calculateProgress() {
-    switch (page) {
-      case 0:
-        return "w-0";
-      case 1:
-        return "w-[12.5%]";
-      case 2:
-        return "w-[25%]";
-      case 3:
-        return "w-[37.5%]";
-      case 4:
-        return "w-[50%]";
-      case 5:
-        return "w-[62.5%]";
-      case 6:
-        return "w-[75%]";
-      case 7:
-        return "w-[87.5%]";
-      case 8:
-        return "w-full";
-      default:
-        return null;
-    }
-  }
-
   return (
     <main className={"bg-primary-cloud min-h-screen py-[180px] relative"}>
       <div
-        className={`${calculateProgress()} absolute top-0 left-0 h-3 bg-primary-sky transition-all`}
+        className={`absolute top-0 left-0 h-3 bg-primary-sky transition-all`}
+        style={{ width: `${(100 / 8) * page}%` }}
       />
       <div className="max-w-[1100px] mx-auto px-5 text-center flex flex-col justify-center items-center">
         <img
-          src={require("../../../assets/logo.png")}
+          src={require("assets/logo.png")}
           alt="Site Logo"
           className="mb-9"
         />
